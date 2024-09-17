bea6c9c80ba01d52fc8846a41be9a47fdb596de4
kaliban <margiottasamuel533@gmail.com>
2024-09-17 13:22:50 +0200

    first commit

---
+++ internal/handlers/api.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+	"github.com/kaliban2056/goapi/internal/middleware"
+)
+
+
+func Handler(r *chi.Mux) {
+	// global middleware
+	r.Use(chimiddle.StripSlashes)
+
+	r.Route("/account", func(router chi.Router) {
+
+		// middleware for /account route
+		router.Use(middleware.Authorization)
+
+		router.Get("/coins", GetCoinBalance)
+	})
+}
